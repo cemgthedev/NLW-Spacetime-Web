9c6e9ce95d6c35e3288b327c7edf5d3cf322034f
cemgthedev <cemg.the.dev@gmail.com>
2023-05-16 22:52:13 -0300

    configurando ambiente para desenvolvimento

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # NLW-Spacetime-Web
 front end da aplicação feita na NLW - Spacetime
+
+# Tecnologias
+
+- Next
+- Tailwindcss
+- ESLint
---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-zinc-950">
+      <h1 className="text-2xl font-semibold text-white">
+        Sua cápsula do tempo
+      </h1>
+    </div>
+  )
+}
