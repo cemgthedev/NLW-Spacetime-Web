853f1ea85e368aa66e729173ac8367f34fcdd8ee
cemgthedev <cemg.the.dev@gmail.com>
2023-05-22 17:30:53 -0300

    finalizando implementação da autenticação

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,14 @@
+import { cookies } from 'next/headers'
+
 import { Copyright } from '@/components/Copyright'
 import { EmptyMemories } from '@/components/EmptyMemories'
 import { Hero } from '@/components/Hero'
 import { SingIn } from '@/components/SingIn'
+import { Profile } from '@/components/Profile'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
@@ -16,7 +21,7 @@ export default function Home() {
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
         {/* Sign In */}
-        <SingIn />
+        {isAuthenticated ? <Profile /> : <SingIn />}
 
         {/* Hero */}
         <Hero />
